556e01723938863ac3705341ae0d1e52d1a37ac7
Aueld <44721590+Aueld@users.noreply.github.com>
2023-09-04 19:46:53 +0900

    Update Unit.h

--- Framework/Game/Unit/Unit.h
+++ Framework/Game/Unit/Unit.h
@@ -1,7 +1,31 @@
 #pragma once
 
-#include "Utilities/BoundingBox.h"
+#include "Geomatries/AnimationRect.h"
+#include "Utilities/Animator.h"
+#include "Game/Components/Movement.h"
 
 class Unit
-{};
+{
+public:
+	Unit(Vector3 position, Vector3 size);
+	~Unit();
+
+	virtual void SetHp(uint hp);
+	virtual void Attacked(Unit* unit);
+	virtual void Dead();
+
+	virtual void Update() = 0;
+	virtual void Render() = 0;
+
+protected:
+	uint HP;
+	uint Attack;
+
+	BoundingBox* box = nullptr;
+	BoundingBox* plat = nullptr;
+	BoundingBox* attackRange = nullptr;
+
+	AnimationRect* animRect = nullptr;
+	Animator* animator = nullptr;
+};
 
