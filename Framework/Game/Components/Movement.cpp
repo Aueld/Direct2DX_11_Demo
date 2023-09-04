7382db22476c03c9e1ae773f9a6b00c200b410fc
Aueld <44721590+Aueld@users.noreply.github.com>
2023-09-04 19:47:30 +0900

    Update Movement.cpp

--- Framework/Game/Components/Movement.cpp
+++ Framework/Game/Components/Movement.cpp
@@ -2,10 +2,19 @@
 #include "Movement.h"
 
 Movement::Movement()
-{}
+{
+
+}
 
 Movement::~Movement()
-{}
+{
 
-void Movement::Update(Vector3 * position)
-{}
+}
+
+void Movement::Update(Vector3& position)
+{
+	float delta = Time::Delta();
+	speed = gravity * delta * 100.0f;
+
+	position -= Vector3(position.x, speed, position.z);
+}
