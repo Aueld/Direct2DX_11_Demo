3d80a4ba57ce4e4cbbf39e13d1848986a11885de
Aueld <44721590+Aueld@users.noreply.github.com>
2023-09-04 19:47:22 +0900

    Update Movement.h

--- Framework/Game/Components/Movement.h
+++ Framework/Game/Components/Movement.h
@@ -6,11 +6,11 @@ public:
 	Movement();
 	~Movement();
 
-	void Update(Vector3* position);
+	void Update(Vector3& position);
 
 private:
 	Vector2 speed = Values::ZeroVec2;
 	
-	float gravity = 9.8f;
+	float gravity = 2.8f;
 	float jumpVelocity = 100.0f;
 };
