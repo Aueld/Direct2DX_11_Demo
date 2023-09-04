a2a810c4040cce5756c42711853cc80c51401486
Aueld <44721590+Aueld@users.noreply.github.com>
2023-09-04 19:47:04 +0900

    Update Unit.cpp

--- Framework/Game/Unit/Unit.cpp
+++ Framework/Game/Unit/Unit.cpp
@@ -1,2 +1,29 @@
 #include "Framework.h"
 #include "Unit.h"
+
+Unit::Unit(Vector3 position, Vector3 size)
+{
+	animRect = new AnimationRect(position, size);
+	animator = new Animator();
+}
+
+Unit::~Unit()
+{
+	SAFE_DELETE(animRect);
+	SAFE_DELETE(animator);
+}
+
+void Unit::SetHp(uint hp)
+{}
+
+void Unit::Attacked(Unit * unit)
+{}
+
+void Unit::Dead()
+{}
+
+void Unit::Update()
+{}
+
+void Unit::Render()
+{}
